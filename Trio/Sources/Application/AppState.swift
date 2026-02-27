d54393cce27f13cf14d9a5ff98ed361088d834c4
Joseph Pantano <j.pantano@outlook.com>
2026-02-27 05:27:34 -0500

    Update AppState.swift

--- Trio/Sources/Application/AppState.swift
+++ Trio/Sources/Application/AppState.swift
@@ -7,7 +7,7 @@ import UIKit
     func trioBackgroundColor(for colorScheme: ColorScheme) -> LinearGradient {
         colorScheme == .dark
             ? LinearGradient(
-                gradient: Gradient(colors: [Color.black.opacity(0.8), Color.black]),
+                gradient: Gradient(colors: [Color.bgDarkBlue, Color.bgDarkerDarkBlue]),
                 startPoint: .top,
                 endPoint: .bottom
             )
